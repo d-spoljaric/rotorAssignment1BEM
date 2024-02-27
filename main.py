53ec4e1a7c36277d980862cecfade1f79572620c
Dominik Špoljarić <d.spoljaric@student.tudelft.nl>
2024-02-27 18:46:13 +0100

    Made some classes

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class airfoil():
+    def __init__(self, ):
+
+
+class blade():
+    def __init__(self):
+
+class turbine():
+    def __init__(self, ):
+
+class bem_simulation():
+    def __init__(self):
